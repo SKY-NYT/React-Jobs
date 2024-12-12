4483e97ef8ce10b2fcee5fb5b15e99d160bb8e73
SKY-NYT <Fiko8464@gmail.com>
2024-12-12 21:27:43 +0000

    My first project on react course

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,8 @@
+import Joblistings from '../components/Joblistings'
+import React from 'react'
+
+const JobsPage = () => {
+  return <section className='bg-blue-50 px-4 py-6'> <Joblistings/></section>  
+};
+
+export default JobsPage
